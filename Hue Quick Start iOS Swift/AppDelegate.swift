e7bbade77fdb2854eadb760713ca7c7392c751bd
Kevin Dew <kev@redbullet.co.uk>
2015-01-22 01:33:38 +0000

    Initial Commit

--- Hue Quick Start iOS Swift/AppDelegate.swift
+++ Hue Quick Start iOS Swift/AppDelegate.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
-import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let phHueSdk:PHHueSDK = PHHueSDK()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        phHueSdk.startUpSDK()
+        phHueSdk.enableLogging(true)
+        let notificationManager = PHNotificationManager.defaultManager()
+        notificationManager.registerObject(self, withSelector: "localConnection" , forNotification: LOCAL_CONNECTION_NOTIFICATION)
+        notificationManager.registerObject(self, withSelector: "noLocalConnection", forNotification: NO_LOCAL_CONNECTION_NOTIFICATION)
+        notificationManager.registerObject(self, withSelector: "notAuthenticated", forNotification: NO_LOCAL_AUTHENTICATION_NOTIFICATION)
+        enableLocalHeartbeat()
         return true
     }
 
@@ -41,71 +46,75 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
     }
-
-    // MARK: - Core Data stack
-
-    lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "me.kevindew.Hue_Quick_Start_iOS_Swift" in the application's documents Application Support directory.
-        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1] as NSURL
-    }()
-
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("Hue_Quick_Start_iOS_Swift", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)!
-    }()
-
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
-        // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
-        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("Hue_Quick_Start_iOS_Swift.sqlite")
-        var error: NSError? = nil
-        var failureReason = "There was an error creating or loading the application's saved data."
-        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
-            coordinator = nil
-            // Report any error we got.
-            let dict = NSMutableDictionary()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
-            dict[NSUnderlyingErrorKey] = error
-            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(error), \(error!.userInfo)")
-            abort()
-        }
+    
+    // MARK: HueSDK
+    
+    func localConnection() {
         
-        return coordinator
-    }()
-
-    lazy var managedObjectContext: NSManagedObjectContext? = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        if coordinator == nil {
-            return nil
-        }
-        var managedObjectContext = NSManagedObjectContext()
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        if let moc = self.managedObjectContext {
-            var error: NSError? = nil
-            if moc.hasChanges && !moc.save(&error) {
-                // Replace this implementation with code to handle the error appropriately.
-                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                NSLog("Unresolved error \(error), \(error!.userInfo)")
-                abort()
-            }
-        }
     }
+    
+    func noLocalConnection() {
+        
+    }
+    
+    func notAuthenticated() {
+        
+    }
+    
+    func checkConnectionState() {
+        
+    }
+    
+    func showNoConnectionDialog() {
+        
+    }
+    
+    // MARK: Heartbeat control
+    
+    // Starts the local heartbeat with a 10 second interval
+    func enableLocalHeartbeat() {
+    }
+    
+    // Stops the local heartbeat
+    func disableLocalHeartbeat() {
 
+    }
+    
+    // MARK: Bridge searching and selection
+    
+    // Search for bridges using UPnP and portal discovery, shows results to user or gives error when none found.
+    func searchForBridgeLocal() {
+
+    }
+    
+    // Delegate method for BridgeSelectionViewController which is invoked when a bridge is selected
+    func bridgeSelectedWithIpAddress(ipAddress:String, andMacAddress macAddress:String) {
+    }
+    
+    // MARK: Bridge authentication
+    
+    // Start the local authentication process
+    func doAuthentication() {
+    }
+    
+    // Delegate method for PHBridgePushLinkViewController which is invoked if the pushlinking was successfull
+    func pushlinkSuccess() {
+    }
+    
+    // Delegate method for PHBridgePushLinkViewController which is invoked if the pushlinking was not successfull
+    func pushlinkFailed(error: PHError) {
+    }
+    
+    // MARK: Alertview delegate
+    
+    func alertView(alertView:UIAlertView, clickedButtonAtIndex buttonIndex:Int) {
+    }
+    
+    // MARK: - Loading view
+    
+    // Shows an overlay over the whole screen with a black box with spinner and loading text in the middle
+    func showLoadingViewWithText(text:String) {
+    }
 }
 
---
+++ Hue Quick Start iOS Swift/ControlLightsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ControlLightsViewController.swift
+//  Hue Quick Start iOS Swift
+//
+//  Created by Kevin Dew on 22/01/2015.
+//  Copyright (c) 2015 KevinDew. All rights reserved.
+//
+
+import UIKit
+
+class ControlLightsViewController: UIViewController {
+    
+    let maxHue = 65535
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "QuickStart"
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func edgesForExtendedLayout() -> UIRectEdge {
+        return (UIRectEdge.Left | UIRectEdge.Bottom | UIRectEdge.Right)
+    }
+    
+}
